f59dc7d6b9f2ba69fb523acdc0ade9ae6da91cc7
José Luis Lafuente <jl@lafuente.dev>
2024-11-26 22:17:51 +0100

    Init

---
+++ README.md
@@ -0,0 +1,4 @@
+# bb-pkgs
+
+Nix packages build with Babashka, see
+[clj-nix](https://jlesquembre.github.io/clj-nix/)
---
+++ flake.nix
@@ -0,0 +1,55 @@
+{
+  description = "Nix packages build with Nix";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    clj-nix = {
+      url = "github:jlesquembre/clj-nix/bb-env";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { self, nixpkgs, clj-nix, ... }@inputs:
+
+    let
+      supportedSystems = [
+        "aarch64-darwin"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "x86_64-linux"
+      ];
+
+      eachSystem = f: nixpkgs.lib.genAttrs supportedSystems (system: f {
+        pkgs = import nixpkgs
+          {
+            inherit system;
+            overlays = [ ];
+          };
+        inherit system;
+      });
+    in
+    {
+      packages = eachSystem ({ pkgs, system }:
+        let
+          mkBabashkaDerivation = clj-nix.outputs.packages.${system}.babashkaEnv {
+            inherit system pkgs;
+            bb-pkgs = self.outputs.packages.${system};
+          };
+        in
+        {
+
+          hello = mkBabashkaDerivation {
+            pkg = ./pkgs/hello;
+          };
+          hello-override = mkBabashkaDerivation {
+            pkg = ./pkgs/hello;
+            override = ./pkgs/hello_override;
+          };
+          simple = mkBabashkaDerivation {
+            pkg = ./pkgs/simple;
+          };
+
+        });
+
+    };
+}
